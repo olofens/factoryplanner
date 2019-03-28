7127723166b1f8d59a3137f30ed6ba573b181d6d
Olof Enström <olofens@student.chalmers.se>
2019-03-28 13:35:19 +0100

    implement react-dnd example i want to use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DragDropContextProvider } from "react-dnd";
+import HTML5Backend from 'react-dnd-html5-backend';
+import Example from "./dndexample.jsx";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <DragDropContextProvider backend={HTML5Backend}>
+					<Example />
+				</DragDropContextProvider>
       </div>
     );
   }
